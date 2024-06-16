dbaf26ad70c7c2f655f482961e08fe6bf9c9265f
abdul-wahab oyekan <oyekanwahab@gmail.com>
2024-06-16 06:11:25 +0000

    chore: Update social media links and add animation to icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Oyekan Abdul-wahab",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "oyekanwahab@gmail.com .com",
+  gitHub: "oyekanwahab",
+  instagram: "oyekanwahab",
+  linkedIn: "oyekanwahab",
+  medium: "oyekanwahab",
+  twitter: "oyekanwahab",
+  youTube: "oyekanwahab",
 };
 
 const primaryColor = "#4E567E";
